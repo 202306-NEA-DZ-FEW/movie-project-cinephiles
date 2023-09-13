a3800833f980ae9e394b6ee46110ebb4657bbe95
Imane Belaid <belaidimane@gmail.com>
2023-09-13 21:08:17 +0100

    fix(project): test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react"
+import Image from "next/image"
 export default function Navbar() {
   return (
-    <nav className= "fixed w-full h-24 shadow-xl text-[22px]">
+    <nav className="fixed w-full h-24 shadow-xl text-[22px]">
       <div>
         <div>
-          <Image
-          
-          />
+          <Image />
         </div>
         <div>
-        <ul classeName= "flex-none">
+          <ul classeName="flex-none">
             <li> About </li>
             <li> Movies </li>
             <li> Actors </li>
-        </ul>
+          </ul>
         </div>
       </div>
-        
     </nav>
   )
 }
