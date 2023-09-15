3d154680fb516b277dcdf4b96ae9300e58ed271f
Lilia <liliabendjeddou9@gmail.com>
2023-09-15 20:51:35 +0100

    feat(component): add carousel to the home page

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect } from "react"
+import React from "react"
 import useEmblaCarousel from "embla-carousel-react"
 
-const EmblaCarousel = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
-
-  useEffect(() => {
-    if (emblaApi) {
-      console.log(emblaApi.slideNodes()) // Access API
-    }
-  }, [emblaApi])
+const EmblaCarousel = ({ latest }) => {
+  const [emblaRef] = useEmblaCarousel({ loop: false })
 
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
+        {latest.results.map((movie) => (
+          <div className="embla__slide" key={movie.id}>
+            <div className="carousel-slide relative">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                alt={`${movie.title} Poster`}
+                className="w-full"
+              />
+              <div className="carousel-overlay absolute inset-0 flex flex-col align-middle bg-opacity-70 bg-black text-white p-4 w-96">
+                <h1 className="text-3xl font-bold">{movie.title}</h1>
+                <p className="text-lg">{movie.overview}</p>
+                <p className="text-lg">
+                  Rated :{movie.vote_average}
+                  <span className="text-yellow-400 pr-1">&#9733;</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
 }
+
 export default EmblaCarousel
