b55bb6f7ca49b38ceb71f2f0d7a26d233bc71a47
Bolphunga <aymendj@yahoo.com>
2023-09-15 18:53:35 +0200

    feat: fixed missing brackets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
+import Carousel from "../components/Carousel/Carousel"
 import Navbar from "@/components/Navbar/Navbar"
 
 export default function Home() {
   return (
     <main>
+      <Carousel />
       <Navbar />
     </main>
   )
