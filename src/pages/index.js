f49b7232619558d35e1b8f3e76a827060e8d3add
darinetag <tagdarinefatma@gmail.com>
2023-09-16 20:46:02 +0100

    feat(card component): create an animated flip card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,18 +46,6 @@ export default function Home() {
         <Grid item xs={9} sm={3} md={1.45}>
           <Card />
         </Grid>
-        <Grid item xs={9} sm={3} md={1.45}>
-          <Card />
-        </Grid>
-        <Grid item xs={9} sm={3} md={1.45}>
-          <Card />
-        </Grid>
-        <Grid item xs={9} sm={3} md={1.45}>
-          <Card />
-        </Grid>
-        <Grid item xs={9} sm={3} md={1.45}>
-          <Card />
-        </Grid>
       </Grid>
     </main>
   )
