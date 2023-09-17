b28aa1d3d2511d7aabfcc96fd9c3cc9548b3d8ef
moh1med <mohamedch047@gmail.com>
2023-09-17 14:26:33 +0100

    style: make carousel autoplay and add pre/next buttons

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import useEmblaCarousel from "embla-carousel-react"
+import Autoplay from "embla-carousel-autoplay"
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs"
+
+const autoplayOptions = {
+  delay: 4000,
+  rootNode: (emblaRoot) => emblaRoot.parentElement,
+}
 
 const EmblaCarousel = ({ latest }) => {
-  const [emblaRef] = useEmblaCarousel({ loop: false })
-
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay(autoplayOptions),
+  ])
   return (
-    <div className="embla" ref={emblaRef}>
+    <div className="embla group" ref={emblaRef}>
       <div className="embla__container">
         {latest.results.map((movie) => (
           <div className="embla__slide" key={movie.id}>
@@ -15,6 +23,7 @@ const EmblaCarousel = ({ latest }) => {
                 alt={`${movie.title} Poster`}
                 className="w-full"
               />
+
               <div className="carousel-overlay absolute inset-0 flex flex-col align-middle bg-opacity-70 bg-black text-white p-4 w-96">
                 <h1 className="text-3xl font-bold">{movie.title}</h1>
                 <p className="text-lg">{movie.overview}</p>
@@ -22,6 +31,18 @@ const EmblaCarousel = ({ latest }) => {
                   Rated :{movie.vote_average}
                   <span className="text-yellow-400 pr-1">&#9733;</span>
                 </p>
+
+                {/* left arrow */}
+
+                <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                  <BsChevronCompactLeft size={30} />
+                </div>
+
+                {/* right arrow */}
+
+                <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                  <BsChevronCompactRight size={30} />
+                </div>
               </div>
             </div>
           </div>
