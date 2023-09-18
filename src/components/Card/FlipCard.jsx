848cb5ddaa1a0e743d5fa0b205d003ae752488de
Bolphunga <aymendj@yahoo.com>
2023-09-18 18:24:05 +0200

    feat: footer

---
+++ src/components/Card/FlipCard.jsx
@@ -0,0 +1,44 @@
+import Card from "@/components/Card/Card"
+import { Grid } from "@mui/material"
+
+;<Grid container>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+  <Grid item xs={9} sm={3} md={1.45}>
+    <Card />
+  </Grid>
+</Grid>
